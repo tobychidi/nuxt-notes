62c027c7fc019c5abb6c86122299222e83869399
Toby Chidi <111127651+tobychidi@users.noreply.github.com>
2023-07-25 18:33:53 +0100

    update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,25 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
    modules: [
-      '@formkit/nuxt',
-      '@vueuse/nuxt',
-      '@pinia/nuxt',
-      'nuxt-icons',
-      '@nuxt/image-edge',
-      '@vueuse/motion/nuxt',
-      'floating-vue/nuxt',
+      "@formkit/nuxt",
+      "@vueuse/nuxt",
+      "@pinia/nuxt",
+      "nuxt-icons",
+      "@nuxt/image-edge",
+      "@vueuse/motion/nuxt",
+      "floating-vue/nuxt",
    ],
-   css: [
-      'assets/styles/style.scss',
-   ],
+   css: ["assets/styles/style.scss"],
    runtimeConfig: {
-      secretKeyP: '',
-      secretKeyR: '',
+      secretKeyP: "",
+      secretKeyR: "",
    },
    imports: {
-      dirs: ['stores'],
+      dirs: ["stores"],
    },
    vue: {
       compilerOptions: {
-         isCustomElement: (tag) => tag.includes('math-field')
-      }
-   }
-})
+         isCustomElement: (tag) => tag.includes("math-field"),
+      },
+   },
+});
